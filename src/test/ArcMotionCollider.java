6caaef20a0a11942a71b4577a5f3af74ff14b3e4
e-271 <estherrobb2718@gmail.com>
2015-02-12 01:34:08 -0500

    name change

---
+++ src/test/ArcMotionCollider.java
@@ -0,0 +1,63 @@
+package collider;
+
+import javax.swing.JFrame;
+
+
+public class ArcMotionCollider {
+	
+	public static void main(String[] args)
+		throws InterruptedException {
+		
+		ArcMotionCollider panel = new ArcMotionColliderPanel();
+		JFrame frame = new JFrame();
+		
+		int attackRange = 20;
+		float attackVelocity = 75; // pixels / sec, clockwise rotation
+		float v = attackVelocity / attackRange; // v / r
+		float r = attackRange;
+		
+		
+
+		//Set initial position based on direction
+		double[] attackPos = new double[2];
+		int[] initialPos = {100 , 100};
+		panel.x = 118; panel.y = 100; panel.width = 20; panel.height = 20; 
+		panel.attackX = (int)attackPos[0]; panel.attackY = (int)attackPos[1];
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		frame.setSize(400 , 400);
+		frame.setVisible(true);
+
+		Thread.sleep(500);
+		long t1 = System.currentTimeMillis(); //t1 in sec
+		boolean collisionDetected = false;
+		int detectionTime=0;
+		long impactCount = 0;
+		
+		while(System.currentTimeMillis() - t1 < 1000){
+			//Parametric eqn for a circle: r*cos(vt / r) , r*sin(vt / r) 
+			//Switching cos to y and sin to x. It should work on the same principle as an x-axis transformation but simpler to deal with.
+			//EFFECTIVE RANGE OF ATTACK: max value of attackPos[0] or attackPos[1]. Try to make it hit radius.
+			//FIX THIS SHIT MAKE IT WORK
+			attackPos[0] = (float)(r * Math.sin(v * (System.currentTimeMillis() - t1) / 1000) + initialPos[0]); 
+			attackPos[1] = (float)(r * Math.cos(v * (System.currentTimeMillis() - t1) / 1000) + initialPos[1]);
+			
+			
+			float x1 = 110, x2 = 138;
+			float y1 =  100, y2 = 120;
+			
+			panel.x = (int)x1; panel.y = (int)y1; panel.width = (int)(x2 - x1); panel.height = (int)(y2 - y1); 
+			panel.attackX = (int)attackPos[0]; panel.attackY = (int)attackPos[1];
+			
+			panel.repaint();
+			
+			if(attackPos[0] >  x1 && attackPos[0] < x2 && attackPos[1] > y1 && attackPos[1] < y2 && ((System.currentTimeMillis() - t1) - detectionTime) > 20){
+				impactCount++;
+				collisionDetected = true; detectionTime = (int) (System.currentTimeMillis() - t1); 
+			}
+			
+		}
+		System.out.printf("Collision detected: %b Time: %d ms Impact Count: %d", collisionDetected, detectionTime, impactCount);
+	}
+}
---
+++ src/test/ArcMotionColliderPanel.java
@@ -0,0 +1,22 @@
+package collider;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class ArcMotionColliderPanel extends JPanel {
+	
+	
+	int attackX, attackY; 
+	int x, y, width, height;
+	
+	public void paintComponent( Graphics g ) {
+		super.paintComponent(g);
+		g.fillRect(x, y, width, height);
+		g.setColor(Color.MAGENTA);
+		g.fillRect(attackX, attackY, 4, 4);
+		
+	}
+
+}
